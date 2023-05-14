e2102242b068d5f2867cec1d2a55a7591602dc59
rlgus3351 <rlgus3351@naver.com>
2023-05-14 21:08:48 +0900

    23/05/14

--- test.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-
-image_path = 'F:/reusable-trash-images/selectstar-reusable-trash-image/3403540.jpg'
-image = cv2.imread(image_path, cv2.IMREAD_COLOR)
-
-cv2.circle(image, (926,1682), 25, (0,0,0),3)
-cv2.circle(image, (1247,2283), 25, (0,0,255),3)
-cv2.imwrite('test.png',image)
