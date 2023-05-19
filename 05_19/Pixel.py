93980937ac9bcebc8978a3c19fd46c1b5c79e844
unknown <rlgus3351@naver.com>
2023-05-19 17:51:40 +0900

    23/05/19

---
+++ 05_19/Pixel.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+import cv2
+
+#-------------------------------------Data Load ----------------------------------------------#
+
+data = pd.read_csv('./data(05_18).csv')
+
+temp = '3403580.jpg'
+path = "E:/reusable-trash-images/selectstar-reusable-trash-image"
+image_path = path + "/" + temp
+
+image = cv2.imread(image_path,cv2.IMREAD_COLOR)
+
+
+print(data[data['imagePath']=='343580'])
+
+#-------------------------------------Image Load ----------------------------------------------#
+image = cv2.resize(image, (1280, 960))
+
+# cv2.imshow("image", image)
+# cv2.waitKey(0)
+
+
+
+
+
+
+
